a4af01d5a889532158c6b25bc9dbf5ff4f74faa7
eserna1 <eserna1@cuc.edu.co>
2024-09-24 12:42:18 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import MiLogo from "../src/assets/img/chase_badge_canine_patrol_paw_patrol_icon_263831.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={MiLogo} alt="Logo React" />
+        <h1>Hola mundo</h1>
+        <p>Este es el proyecto 03</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
